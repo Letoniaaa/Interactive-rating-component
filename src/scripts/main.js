3d1ad928e78100d8dfae6cfc437fed4be2450f00
Letoniaaa <leticiapink2015@hotmail.com>
2024-05-03 23:48:08 -0300

    Finish

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,4 +3,36 @@ const btn2 = document.getElementById('btn2')
 const btn3 = document.getElementById('btn3')
 const btn4 = document.getElementById('btn4')
 const btn5 = document.getElementById('btn5')
+const sub = document.querySelector('.submit')
+const span = document.querySelector('#sp')
+const pricipal = document.querySelector('.principal')
+const outra = document.querySelector('.outra')
+let flag
+
+
+btn1.addEventListener('click', () => {
+    flag = ' 1 '
+})
+btn2.addEventListener('click', () => {
+    flag = ' 2 '
+})
+btn3.addEventListener('click', () => {
+    flag = ' 3 '
+})
+btn4.addEventListener('click', () => {
+    flag = ' 4 '
+})
+btn5.addEventListener('click', () => {
+    flag = ' 5 '
+    
+})
+
+
+
+
+sub.addEventListener('click', () => {
+    pricipal.style.display = 'none'
+    outra.style.display = 'flex'
+    span.innerText = flag
+})
 
